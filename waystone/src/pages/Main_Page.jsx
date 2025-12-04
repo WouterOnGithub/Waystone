bd169c5f3fb90848ce54bc5e986aeebe8356e599
F0RD-P1N3S15 <bridget.parish@student.ehb.be>
2025-12-04 12:20:33 +0100

    Adding App.jsx +Placeholders

---
+++ waystone/src/pages/Main_Page.jsx
@@ -0,0 +1,42 @@
+/* Not completed, still testing */
+import React from "react";
+import "./CSS.css";
+
+export default function MainPage() 
+{
+  return (
+    <div>
+      {/* Sidebar */}
+      <nav>
+        <img src="/" alt="Waystone_Logo" />
+        <a href="/">account</a>
+        <a href="/">My Campaigns</a>
+        <a href="/">New Campaign</a>
+        <a href="/">settings</a>
+        <img src="/" alt="Settings_Logo" />
+        <a href="/">help</a>
+      </nav>
+
+      {/* Main Section */}
+      <div>
+        <div id="title">
+          <p>Welcome</p>
+        </div>
+
+        <div id="content">
+          <p>Recent Campaigns</p>
+
+          <div id="box-section">
+            <div id="box-text">
+              <p>Project_name</p>
+              <div id="box"></div>
+            </div>
+          </div>
+
+          <p>News</p>
+          {/* News section can be expanded later */}
+        </div>
+      </div>
+    </div>
+  );
+}
