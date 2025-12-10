2a9fef919bf10080b0e6ea868df0dda08b1f0d84
WouterOnGithub <wouter.van.den.bossche1@student.ehb.be>
2025-12-10 15:50:27 +0100

    Added footer, header and sidebar

---
+++ waystoneProject/src/components/UI/Sidebar.jsx
@@ -0,0 +1,25 @@
+import { Link } from 'react-router-dom';
+import Waystone_Logo from '../assets/Waystone_Logo.png'; // adjust path if needed
+
+const Sidebar = () => {
+  return (
+    <div className="navigation">
+      <nav>
+        <br />
+        <img src={Waystone_Logo} alt="Waystone Logo" id="Waystone_Logo" />
+        <br /><br /><br />
+        <Link to="/">Account</Link>
+        <br /><br />
+        <Link to="/">My Campaigns</Link>
+        <br /><br />
+        <Link to="/">New Campaign</Link>
+        <br /><br />
+        <Link to="/">Settings</Link>
+        <br /><br />
+        <Link to="/">Help</Link>
+      </nav>
+    </div>
+  );
+};
+
+export default Sidebar;
