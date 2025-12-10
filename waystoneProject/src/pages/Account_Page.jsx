745de7b17ac7a9fdbad82f3f30c584f9f381efd4
F0RD-P1N3S15 <bridget.parish@student.ehb.be>
2025-12-10 13:19:15 +0100

    Main CSS edited

--- waystoneProject/src/pages/Account_Page.jsx
+++ waystoneProject/src/pages/Account_Page.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import "./pages-css/CSS.css";
 import "./pages-css/Account_Page.css";
 import Waystone_Logo from "../assets/PlaceholderImage.jpg";
+import Edit_Logo from "../assets/PlaceholderImage.jpg";
 import Placeholder from "../assets/PlaceholderImage.jpg";
 
 function Main_Page() 
@@ -36,12 +37,45 @@ function Main_Page()
       <div id="main">
         {/* The green bar at the top of the page */}
         <div id="title">
-          <p>Account</p>
+            <p>Account</p>
         </div>
         {/* The account section */}
         <div id="content">
+            {/* Main account section */}
             <div id="account-box">
-                <p>placeholder</p>
+                <div>
+                    <img src={Placeholder} alt="Account_Profile" id="Account_Profile" />
+                    <img src={Edit_Logo} alt="Edit_Logo" id="Edit_Logo" />
+                </div>
+                <div>
+                    <b>Username_Placeholder</b>
+                    <p>
+                        This is a very long description that displays whatever text the account user put in, this will be repeated.
+                        This is a very long description that displays whatever text the account user put in, this will be repeated.
+                        This is a very long description that displays whatever text the account user put in, this will be repeated.
+                    </p>
+                </div>
+            </div>
+
+            {/* Account stats section */}
+            <div id="account-box">
+                <div>
+                    <p><b>Total campaigns: </b> 0 </p> {/* Number later to be replaced */}
+                    <p><b>Last played: </b> Project_name </p> {/* Name later to be replaced */}
+                </div>
+            </div>
+
+            <button id="button-green">Archived Campaigns</button>
+            <br />
+            <br />
+            <div id="account-notes">
+                <b>Notes</b>
+                <form>
+                    <textarea placeholder="Enter you notes here ..."></textarea>
+                    <br />
+                    <br />
+                    <button id="button-green">Save</button>
+                </form>
             </div>
         </div>
       </div>
