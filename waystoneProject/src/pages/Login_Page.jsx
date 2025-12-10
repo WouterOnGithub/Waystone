75e26fc4957a895d9ea91af16e4b933d44ad2e97
Quinten Lauwers <99260831+Sjarel47@users.noreply.github.com>
2025-12-10 13:31:22 +0100

    jsx file voor login pagina aangepast en een foto voor de login toegevoegd

--- waystoneProject/src/pages/Login_Page.jsx
+++ waystoneProject/src/pages/Login_Page.jsx
@@ -1,1 +1,48 @@
-/* Not completed */
+import React from "react";
+import { Link } from "react-router-dom";
+import "./pages-css/CSS.css";
+import Waystone_Logo from "../assets/login_image.webp";
+
+function Login_Page() {
+  return (
+    <div id="login-container">
+
+      {/* Left side: login form */}
+      <div id="login-section">
+        <h1 id="login-title">Login</h1>
+
+        <input
+          type="text"
+          placeholder="Example_Username"
+          className="login-input"
+        />
+
+        <input
+          type="email"
+          placeholder="Example_Email"
+          className="login-input"
+        />
+
+        <input
+          type="password"
+          placeholder="************"
+          className="login-input"
+        />
+
+        <Link to="/register" id="login-register-link">
+          Don't have an account? Create one here!
+        </Link>
+
+        <button id="login-button">Enter</button>
+      </div>
+
+      {/* Right side: image background + logo */}
+      <div id="login-image-section">
+        <img src={Waystone_Logo} alt="Waystone Logo" id="login-logo" />
+      </div>
+
+    </div>
+  );
+}
+
+export default Login_Page;
