00153f8e82c2c0b5169115d4eccd9eefd489e65e
F0RD-P1N3S15 <bridget.parish@student.ehb.be>
2025-12-11 15:23:22 +0100

    Updated pages

--- waystoneProject/src/pages/Account_Page.jsx
+++ waystoneProject/src/pages/Account_Page.jsx
@@ -6,6 +6,11 @@ import "./pages-css/Account_Page.css";
 import Header from "../components/UI/Header";
 import Footer from "../components/UI/Footer";
 import Sidebar from "../components/UI/Sidebar";
+import Waystone_Logo from "../assets/PlaceholderImage.jpg";
+import UploadIMG_Logo from "../assets/PlaceholderImage.jpg";
+import Required_Logo from "../assets/Required_Logo.webp";
+import Delete_Logo from "../assets/Delete_Logo.webp";
+import Add_Logo from "../assets/Add_Logo.webp";
 import Placeholder from "../assets/PlaceholderImage.jpg";
 
 function Account_Page() {
--- waystoneProject/src/pages/My_Campaigns_Page.jsx
+++ waystoneProject/src/pages/My_Campaigns_Page.jsx
@@ -3,6 +3,12 @@ import "./pages-css/CSS.css";
 import Header from "../components/UI/Header";
 import Footer from "../components/UI/Footer";
 import Sidebar from "../components/UI/Sidebar";
+import Waystone_Logo from "../assets/PlaceholderImage.jpg";
+import UploadIMG_Logo from "../assets/PlaceholderImage.jpg";
+import Required_Logo from "../assets/Required_Logo.webp";
+import Delete_Logo from "../assets/Delete_Logo.webp";
+import Add_Logo from "../assets/Add_Logo.webp";
+import Placeholder from "../assets/PlaceholderImage.jpg";
 
 const campaignSections = [
   {
