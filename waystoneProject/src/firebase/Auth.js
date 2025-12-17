17585506da54c28ad5ed5a681a4b7408d0ae3c34
Argo216 <nday.roman@gmail.com>
2025-12-17 14:03:32 +0100

    Update Auth.js

--- waystoneProject/src/firebase/Auth.js
+++ waystoneProject/src/firebase/Auth.js
@@ -1,4 +1,4 @@
-import { getAuth, createUserWithEmailAndPassword,signInWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth'
+import { getAuth, createUserWithEmailAndPassword,signInWithEmailAndPassword, onAuthStateChanged, signOut } from 'firebase/auth'
 import {doc, setDoc, serverTimestamp} from 'firebase/firestore'
 import {auth,db} from './firebase'
 
