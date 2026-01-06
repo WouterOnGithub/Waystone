344801e0280595c18494f41b60725288ed2fd25e
Quinten Lauwers <99260831+Sjarel47@users.noreply.github.com>
2026-01-06 13:13:24 +0100

    game settings jsx content

---
+++ waystoneProject/src/components/popups/Game_Settings.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../../pages/pages-css/CSS.css";
+import "../../pages/pages-css/Add_View.css";
+
+function Game_Settings() 
+{
+  return (
+    <div id="addview-page">
+    <div id="addview-box">
+
+      <p id="addview-title">Game Settings</p>
+
+      <div id="addview-content">
+
+        <form id="input-box-gray" className="game-settings-content">
+            <div id="game-settings-options">
+                {/* Functionality still needs to be added */}
+                {/* The buttons of the game settings */}
+                                                {/* onClick navigates back 1 page */}
+                <br /><button id="button-icons" onClick={() => navigate(-1)}>Resume Game</button>
+                <br /><Link to="/user/Game_Settings_SAVEGAME"><button id="button-icons">Save Game</button></Link>
+                {/* I'm not sure if help & tutorial is still supposed to be included, can be deleted if not */}
+                <br /><button id="button-icons">Help & Tutorial</button>
+            </div>
+
+            <br />
+                <button id="button-green">Return to Main Menu</button>
+                
+        </form>
+
+      </div>
+    </div>
+    </div>
+  );
+}
+
+export default Game_Settings;
