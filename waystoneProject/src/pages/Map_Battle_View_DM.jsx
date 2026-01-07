6f38b0695db9a419ca3efbd8b041d7cafbc71611
WouterOnGithub <wouter.van.den.bossche1@student.ehb.be>
2026-01-07 15:23:19 +0100

    preparation

--- waystoneProject/src/pages/Map_Battle_View_DM.jsx
+++ waystoneProject/src/pages/Map_Battle_View_DM.jsx
@@ -2,7 +2,7 @@ import BattleMap from "../components/map/battleMap";
 import BattleMapWithSidebar from "../components/map/battleMapWithSidebar";
 
 function Map_Battle_View_DM() {
-  return <BattleMapWithSidebar userId="UID" campaignId="Campaign_ID" mapId="mapsId" />;
+  return <BattleMapWithSidebar userId="6v5VMJwiBgQjsAMLc42PBe7Krmd2" campaignId="gFOfbenj1aCJX46ZuJm8" mapId="abcdefg" />;
 }
 
 export default Map_Battle_View_DM; 
