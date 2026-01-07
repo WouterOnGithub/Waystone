e2420a877e9f536b0117baeb2b65a183238b0b05
WouterOnGithub <wouter.van.den.bossche1@student.ehb.be>
2026-01-07 14:14:10 +0100

    fixed join session

--- waystoneProject/src/pages/Login_Page.jsx
+++ waystoneProject/src/pages/Login_Page.jsx
@@ -70,7 +70,7 @@ const handleSession= [
         <br /><br />
         <div id="login-register-button">
           <button id="button-green" type="button" onClick={handleLogin}>Enter</button>
-          <button id="button-gray" type="button" onClick={handleSession}>Join Session</button>
+          <button id="button-gray" type="button" onClick={goToSession}>Join Session</button>
         </div>
         {/* An error message in case an error occures */}
         {/*{error && <p style={{ color: "#D34848" }}>{error}</p>}*/}
