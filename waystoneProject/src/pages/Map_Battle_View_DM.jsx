d436ec76d2ee27798857990d1c436496db5910ea
WouterOnGithub <wouter.van.den.bossche1@student.ehb.be>
2026-01-07 13:26:48 +0100

    rename

---
+++ waystoneProject/src/pages/Map_Battle_View_DM.jsx
@@ -0,0 +1,8 @@
+import BattleMap from "../components/map/battleMap";
+import BattleMapWithSidebar from "../components/map/battleMapWithSidebar";
+
+function Map_Battle_View() {
+  return <BattleMapWithSidebar userId="UID" campaignId="Campaign_ID" mapId="mapsId" />;
+}
+
+export default Map_Battle_View; 
